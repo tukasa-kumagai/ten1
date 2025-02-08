44bb31680a37c6f0c75b61b00d6ad61bf825dd36
kumakuma99tukasa <tukasa.ff@gmail.com>
2025-02-08 16:03:31 +0900

    fontの修正

--- WordPressTheme/archive-price.php
+++ WordPressTheme/archive-price.php
@@ -3,7 +3,7 @@
   <div class="lower-page-mv__slide">
     <picture class="lower-page-mv__slide-image">
       <source srcset="<?php echo esc_url(get_theme_file_uri('../dist/assets/images/price/pc-sp-price-mv.jpg')); ?>" media="(min-width: 768px)">
-      <img src="<?php echo esc_url(get_theme_file_uri('../dist/assets/images/price/sp-sp-price-mv.jpg')); ?>" alt="">
+      <img src="<?php echo esc_url(get_theme_file_uri('../dist/assets/images/price/sp-price-mv.jpg')); ?>" alt="">
     </picture>
   </div>
   <div class="lower-page-mv__text">
